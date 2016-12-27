b5b84396ff05014f67cefe2e5c3074cdbe001715
Maxime BUS <bus.maxime@mailoo.org>
2016-12-27 13:52:22 +0100

    Return the number when string is only one number

--- string-calculator/src/main/java/info/busmaxime/stringcalculator/day1/StringCalculator.java
+++ string-calculator/src/main/java/info/busmaxime/stringcalculator/day1/StringCalculator.java
@@ -3,6 +3,13 @@ package info.busmaxime.stringcalculator.day1;
 public class StringCalculator {
 
     public static int add(String expression) {
+        if(notEmpty(expression)) {
+            return Integer.parseInt(expression);
+        }
         return 0;
+    }
+
+    private static boolean notEmpty(String expression) {
+        return !"".equals(expression);
     }
 }
--- string-calculator/src/test/java/info/busmaxime/stringcalculator/day1/StringCalculatorTest.java
+++ string-calculator/src/test/java/info/busmaxime/stringcalculator/day1/StringCalculatorTest.java
@@ -10,4 +10,11 @@ public class StringCalculatorTest {
     public void an_empty_string_should_return_0() throws Exception {
         assertThat(StringCalculator.add("")).isEqualTo(0);
     }
+
+    @Test
+    public void a_string_that_contain_only_one_number_should_return_this_number() throws Exception {
+        assertThat(StringCalculator.add("0")).isEqualTo(0);
+        assertThat(StringCalculator.add("1")).isEqualTo(1);
+        assertThat(StringCalculator.add("2")).isEqualTo(2);
+    }
 }
